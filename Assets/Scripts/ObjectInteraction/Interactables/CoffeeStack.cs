83a794e39586196d63b0e7658e124684e070a39c
Ian <ianmccallsemail@gmail.com>
2026-07-10 14:31:18 -0700

    Infinite Coffee Cups
    infinite coffee cups for when the player loses their cup, interact with the stack of cups next to the coffee machine to get a cup

---
+++ Assets/Scripts/ObjectInteraction/Interactables/CoffeeStack.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class CoffeeStack : MonoBehaviour, IInteractable
+{
+	[SerializeField] private VoidEventChannelSO stopInteraction;
+	
+	[SerializeField] private GameObject emptyCupPrefab;
+	[SerializeField] private InteractableSettingsSO settings;
+	[SerializeField] private Vector3 cupLocation;
+	private GameObject currentItemHeld;
+	private Outline outline;
+	
+	private void Awake()
+	{
+		if (!TryGetComponent<Outline>(out outline))
+        {
+            outline = gameObject.AddComponent<Outline>();
+        }
+        outline.enabled = false;
+        outline.OutlineWidth = settings.OutlineWidth;
+        outline.OutlineMode = Outline.Mode.OutlineVisible;
+	}
+	
+	public float GetInteractDistance()
+    {
+        return settings.InteractionDistance + 3;
+    }
+
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+    
+	public bool CanInteract()
+    {
+        if (currentItemHeld == null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+	
+	public void StartInteract()
+    {
+        stopInteraction.RaiseEvent();
+		GameObject emptyCup = Instantiate(emptyCupPrefab, cupLocation, Quaternion.identity);
+		emptyCup.name = emptyCupPrefab.name;
+    }
+	
+	public void StopInteract()
+    {
+    }
+	
+	    public void StartHover()
+    {
+        outline.OutlineColor = settings.HoverColor;
+        outline.enabled = true;
+    }
+
+    public void StopHover()
+    {
+        outline.enabled = false;
+    }
+    
+	public void SetCurrentItemHeld(GameObject newItemHeld)
+    {
+        currentItemHeld = newItemHeld;
+    }
+}
